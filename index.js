6442ea19c65916865d9f0306ce632c6191f41428
Gustavo Salvi <tatosalvi@gmail.com>
2021-02-12 01:27:01 -0300

    I made this API tester

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("this has started")
-console.log("and this")
+const express = require("express")
+const app = express()
+
+//const
+
+app.get("/", (req, res)=> {
+  res.sendFile(__dirname + "/index.html")
+})
+app.get("/getthis", (req, res)=> {
+  res.send({"res": {obj: "this is my app 2", foo: "bar"}})
+})
+
+app.post("/postme", (req, res)=> {
+  res.send({res: "this has been sent by post"})
+})
+
+app.all((req,res)=> {
+  res.send({
+    error: "404, request not found"
+  })
+})
+
+app.listen(3000)
