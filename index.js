2437e983ea19c9fafe1cab89571e0be4ea9eb24a
Gustavo Salvi <tatosalvi@gmail.com>
2021-02-13 21:49:39 -0300

    router created and API tester improved

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const express = require("express")
 const app = express()
+const router = express.Router()
+const port = 3000
 
-//const
+const pug = require("pug")
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res)=> {
   res.sendFile(__dirname + "/index.html")
 })
-app.get("/getthis", (req, res)=> {
-  res.send({"res": {obj: "this is my app 2", foo: "bar"}})
-})
 
+// router version 1
+app.route("/usr")
+  .get((req,res)=> {
+    console.log("usr readed")
+  })
+  .post((req,res)=> {
+    console.log("usr created")
+  })
+  .put((req,res)=> {
+    console.log("usr updated")
+  })
+
+/*
+  this is for testing the API
+*/
+app.get("/API", (req, res)=> {
+  res.sendFile(__dirname + "/public/API-TESTER.html")
+})
+app.get("/pars/:name/:job/:tech", (req, res)=> {
+  res.send(req.params)
+  console.log
+})
 app.post("/postme", (req, res)=> {
   res.send({res: "this has been sent by post"})
 })
 
-app.all((req,res)=> {
-  res.send({
-    error: "404, request not found"
-  })
-})
 
-app.listen(3000)
+
+app.listen(port, ()=> {
+  console.log("------ Linstening on port " + 3000 + " ------")
+})
